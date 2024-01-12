05bedf4e0ce2f6bc1239a1b58ebfaad2ee51924b
Ashu <ashutoshkhade2530@gamil.com>
2024-01-12 13:36:45 +0530

    new data added

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -36,14 +36,59 @@ class Sll:
     while temp is not None:
       print(temp.item,end=' ')
       temp=temp.next
-      
-
+  def delete_first(self):
+    if self.start is not None:
+      self.start=self.start.next
+  def delete_last(self):
+    if self.start is None:
+      pass
+    elif self.start.next is not None:
+      self.start=None
+    else:
+      temp=self.start
+      while temp.next.next is not None:
+        temp=temp.next
+      temp.next=None
+  def delete_item(self,data):
+    if self.start is None:
+      pass
+    elif self.start.next is None:
+      if self.start.item==data:
+        self.start=None
+    else:
+      temp=self.start
+      if temp.item==data:
+        self.start=temp.next
+      else:
+        while temp.next is not None:
+          if temp.next.item==data:
+            temp.next=temp.next.next
+            break
+          temp=temp.next      
+  def __iter__ (self)  :
+    return Slliterator(self.start)
+class Slliterator:
+  def __init__(self, start):
+    self.current=start
+  def __iter__ (self)  :
+    return self
+  def __next__(self):
+    if not self.current:
+      raise StopIteration
+    data=self.current.item
+    self.current=self.current.next
+    return data
 mylist=Sll() 
 mylist.insert_at_last(30)
 mylist.insert_at_start(20)
 mylist.insert_at_start(10)
 mylist.insert_after(mylist.search(20),25)
-mylist.insert_after(35)
+mylist.insert_after(mylist.search(30),35)
 mylist.print_list()
 print()
- 
+mylist.delete_item(35)
+#mylist.print_list()
+#print()
+for result in mylist:
+  print(result,end=' ')
+print()
